aae8847378a0d54ef9cb3732a5ca725e82c1a297
Gregory A. Wert <gaw8pa@virginia.edu>
2017-09-04 20:46:41 -0400

    uploaded knn model w/ revisions and comments

--- non-parametric-knn.R
+++ non-parametric-knn.R
@@ -1,48 +1,52 @@
-# Ben Greenawald, Abhijith Mandya ,	Gregory Wert
-
 # Kaggle Housing Competition
+# K Nearest Neighbor approach to Non-parametric modelling
+# Ben Greenawald, Abhijith Mandya, Gregory Wert
 
+#load in libraries
 library(tidyverse)
 library(class)
-library(caret)
-
+library('FNN')
 source("preprocess.R")
 
-# This file houses the code for the non-parametric approach
+#set seed
+set.seed(66)
 
-# Get the data in
+#load in train and test sets
 res <- preprocess()
 train <- as.data.frame(res$train)
 test <- as.data.frame(res$test)
 
 #create backup of data in case we need the original
-train_bkup <-  train
-test_bkup <-  test
+train_bkup = train
+test_bkup = test
 
-#drop the response variable from testing
+#drop the response variable from the train set
 train$SalePrice = NULL
 
-#normalize the numeric variables in the train and test sets
-num_vars_train <-  sapply(train, is.numeric)
-train <-train[, num_vars_train==TRUE]
-train <- as.data.frame(lapply(train, scale))
+#drop Ids from train and test case
+train$Id = NULL
+test$Id = NULL
 
-num_vars_test <- sapply(test, is.numeric)
-test <- test[num_vars_test==TRUE]
-test <- as.data.frame(lapply(test, scale))
+#filter out the non numeric variables in the train and test sets
+num_vars_train = sapply(train, is.numeric)
+train = train[num_vars_train==TRUE]
+num_vars_test = sapply(test, is.numeric)
+test = test[num_vars_test==TRUE]
+
+#normalize the numeric variables in the train and test sets
+train = as.data.frame(lapply(train, scale))
+test = as.data.frame(lapply(test, scale))
 
 #run the KNN prediction
-pred_knn <- knn.cv(train, as.vector(train_bkup$SalePrice), k=37)
+pred_knn_base = knn(train, test, train_bkup$SalePrice, k=38)
 
-# Try using knn regression
-pred_knn2 <- knnreg(train, as.vector(train_bkup$SalePrice), k = 37)
-preds <- predict(pred_knn2, test)
+#run a KNN regression prediction
+pred_knn = knn.reg(train, test, train_bkup$SalePrice, k=38)
 
 #organize it into a a dataframe
-results <- data.frame(as.integer(test_bkup$Id), preds)
+results = data.frame(as.integer(test_bkup$Id), pred_knn$pred)
 colnames(results) <- c("Id", "SalePrice")
+
+#write out results to a CSV file
 write_csv(results, "submission_knn.csv")
-
-
-
 
